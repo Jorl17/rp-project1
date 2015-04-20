1d5ba1fe42bc6c80fa9c9ecf2d4b2cae0911f898
Joaquim Leitão <jocaleitao93@gmail.com>
2015-04-20 18:58:25 +0100

    Fixed mvregress

--- fill_missing_values_multivariate.m
+++ fill_missing_values_multivariate.m
@@ -37,31 +37,12 @@ function outdata = fill_missing_values_multivariate(indata, method)
         
         %Call mvregress for the given feature
         [beta,Sigma,E,CovB,logL] = mvregress(current_design_matrix, current_observation_matrix);
-        
-        size(current_design_matrix)
-        size(current_observation_matrix)
-        size(beta)
-        size(Sigma)
-        size(E)
-        fprintf('AAA\n');
-        pause;
-        
-        %FIXME: We can have iterations where the number of missing values
-        %in the current collumn is higher than the number of non-missing
-        %values in that collumn. In that case we cannot apply the
-        %regression like we are doing.... FIXME: SOLVE THIS FOR THE
-        %MENTIONED SITUATIONS!!!!
-        
-        %{
-        
+
         %Apply regression to the missing data and return it
         for i=1:length(current_lines_missing)
-            %yi = xi1 * beta_1 + ... + x1p * beta_p + ei -- CONFIRM!
-            
-            
-            yi = indata(current_lines_missing(i), indexes_without_missing) * beta + E(i);
+            %yi = xi1 * beta_1 + ... + x1p * beta_p
+            yi = indata(current_lines_missing(i), indexes_without_missing) * beta;
             outdata(current_lines_missing(i), j) = yi;
         end
-        %}
     end
 end
