108ee98a37b3031cbde7be4f90372e0e8bee543e
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-04-21 01:02:39 +0100

    Fix dataset (remove ID column)

--- load_dataset.m
+++ load_dataset.m
@@ -1,6 +1,6 @@
 function [ column_names, higgs_data ] = load_dataset()
     
     load('dataset/higgs_data.mat');    
-    higgs_data=higgs_data_for_optimization;
+    higgs_data=higgs_data_for_optimization(:,2:end);
 end
 
