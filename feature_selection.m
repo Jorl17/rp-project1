63262f682974be1e8325190b3c1f2b6f2a26dc54
Joaquim Leitão <jocaleitao93@gmail.com>
2015-04-27 10:34:04 +0100

    Updated feature_selection to support kruskalwallis and corrected typo

--- feature_selection.m
+++ feature_selection.m
@@ -1,10 +1,16 @@
 function [ data, indexes ] = feature_selection(data, classes, method, target_number_of_features, varargin)
     if strcmp(method, 'fisher')
+        %Filter Method
         indexes = fisher(data, classes);
         indexes = indexes(1:target_number_of_features);
         data = data(:,indexes);
     elseif strcmp(method, 'fsfisher')
-        out = fisher(data, classes);
+        %Filter Method
+        out = fsFisher(data, classes);
+        indexes = out.fList(1:target_number_of_features);
+        data = data(:,indexes);
+    elseif strcmp(method, 'fskruskalwallis')
+        out = fsKruskalWallis(data, classes);
         indexes = out.fList(1:target_number_of_features);
         data = data(:,indexes);
     end
