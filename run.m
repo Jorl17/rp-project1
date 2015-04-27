61d96e27086d13a89a9ab3d5621bffb69314d32d
Joaquim Leitão <jocaleitao93@gmail.com>
2015-04-27 23:58:56 +0100

    Added mRMR Multiplicative implementation

--- run.m
+++ run.m
@@ -2,7 +2,10 @@ target_number_of_features = 2;
 [~, higgs_data] = load_dataset();
 higgs_data = fill_missing_values(higgs_data, 'mean');
 sprt_data = convert_to_sprt_data(higgs_data);
+%{
 [data_fisher, indexes_fisher] = feature_selection(sprt_data, 'fisher', target_number_of_features);
 [data_fs_fisher, indexes_fs_fisher] = feature_selection(sprt_data, 'fsfisher', target_number_of_features);
 [data_auc, indexes_auc] = feature_selection(sprt_data, 'auc', target_number_of_features);
-
+%}
+ fea = mrmr_multiplicative(sprt_data, target_number_of_features);
+ fea
