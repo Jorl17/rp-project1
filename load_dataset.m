11b09e235295a188b80f5612972f6b3359ba9b89
Joaquim Leitão <jocaleitao93@gmail.com>
2015-04-27 10:24:25 +0100

    Corrected bug in the load_dataset function (Added in the last commit)

--- load_dataset.m
+++ load_dataset.m
@@ -1,7 +1,7 @@
 function [ column_names, higgs_data, higgs_classes ] = load_dataset()
     
-    load('dataset/higgs_data.mat');    
+    load('dataset/higgs_data.mat');
     higgs_data=higgs_data_for_optimization(:,2:end);
-    higgs_classes=higgs_data_for_optimization(:,1);
+    higgs_classes=higgs_data_for_optimization(:,32);
 end
 
