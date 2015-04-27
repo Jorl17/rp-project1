d13b572bd57c12366ecfad3fd0ba573eae6270cb
Joaquim Leitão <jocaleitao93@gmail.com>
2015-04-27 09:34:03 +0100

    Updated load_dataset function: Now also returning the class of each entry

--- load_dataset.m
+++ load_dataset.m
@@ -1,6 +1,7 @@
-function [ column_names, higgs_data ] = load_dataset()
+function [ column_names, higgs_data, higgs_classes ] = load_dataset()
     
     load('dataset/higgs_data.mat');    
     higgs_data=higgs_data_for_optimization(:,2:end);
+    higgs_classes=higgs_data_for_optimization(:,1);
 end
 
