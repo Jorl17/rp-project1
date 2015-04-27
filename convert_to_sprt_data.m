f712dfcef35c66fb3092827da4a9f250f5ce2d62
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-04-27 15:05:19 +0100

    Make convert_to_sprt_data independent of number of features

--- convert_to_sprt_data.m
+++ convert_to_sprt_data.m
@@ -1,6 +1,6 @@
 function [ sprt_data ] = convert_to_sprt_data( data )
-    sprt_data.X = data(:, 1:30)';
-    sprt_data.y = data(:,31);
+    sprt_data.X = data(:, 1:end-1)';
+    sprt_data.y = data(:,end);
     sprt_data.dim = size(sprt_data.X, 1);
     sprt_data.num_data = size(sprt_data.X, 2);
     sprt_data.name = 'Higgs Data';
