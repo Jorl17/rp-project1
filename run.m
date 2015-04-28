951c10cd6281debe216931908b3d9e731e062f5f
Joaquim Leitão <jocaleitao93@gmail.com>
2015-04-28 01:14:45 +0100

    Finished second mrmr method (Realized I was using the wrong name for the first one so I changed it)

--- run.m
+++ run.m
@@ -6,6 +6,6 @@ sprt_data = convert_to_sprt_data(higgs_data);
 [data_fisher, indexes_fisher] = feature_selection(sprt_data, 'fisher', target_number_of_features);
 [data_fs_fisher, indexes_fs_fisher] = feature_selection(sprt_data, 'fsfisher', target_number_of_features);
 [data_auc, indexes_auc] = feature_selection(sprt_data, 'auc', target_number_of_features);
+[data_mrmra, indexes_mrmra] = feature_selection(sprt_data, 'mrmra', target_number_of_features);
 %}
- fea = mrmr_multiplicative(sprt_data, target_number_of_features);
- fea
+[data_mrmrm, indexes_mrmrm] = feature_selection(sprt_data, 'mrmrm', target_number_of_features);
