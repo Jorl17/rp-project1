6b467880e06fd8a22b8183cddc0d9dce901b69e1
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-05-01 21:46:37 +0100

    FIx load_dataset

--- load_dataset.m
+++ load_dataset.m
@@ -1,6 +1,6 @@
-function [ column_names, higgs_data ] = load_dataset()
-    
+function [ column_names, higgs_data ] = load_dataset()    
     load('dataset/higgs_data.mat');
     higgs_data=higgs_data_for_optimization(:,2:end);
+    column_names = column_names(:,2:end);
 end
 
