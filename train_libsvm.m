7b4ee44abe642acbfaa5680d5dd67a457fecc898
Joaquim Leitão <jocaleitao93@gmail.com>
2015-05-24 19:23:52 +0100

    Added documentation to the training functions. Using the new MATLAB SVM train function (FITCSVM) as svmtrain is going to be deprecated

--- train_libsvm.m
+++ train_libsvm.m
@@ -1,4 +1,17 @@
 function [ svm_model ] = train_libsvm( training_x, training_y, varargin )
+%TRAIN_LIBSVM Trains a LIBSVM classifier with a given set of taining data
+%and labels
+%   Receives the training data in the arrays "training_x" and "training_y"
+%   and trains a MATLAB SVM.
+%   training_x is a m-by-n matrix, where n is the number of features,
+%   containing in each row the feature values for the given training
+%   instance.
+%   training_y is a m-by-1 matrix, containing the classification/label of
+%   each training instance.
+%   Additionally, there is an opcional parameter used to define
+%   the maximum number of iterations of the training process.
+%   This method returns a MATLAB SVMStruct object, containing the trained
+%   classifier.
     
     if (iscell(varargin) && ~isempty(varargin))
         %Convert to string because varargin is a cell array
--- train_svm.m
+++ train_svm.m
@@ -1,17 +1,15 @@
 function [ SVMStruct ] = train_svm( x_data, y_data, varargin )
+%TRAIN_SVM Trains a MATLAB implemented SVM Classifier with a given set of
+%taining data and labels
+%   Receives the training data in the arrays "x_data" and "y_data" and
+%   trains a MATLAB SVM.
+%   x_data is a m-by-n matrix, where n is the number of features,
+%   containing in each row the feature values for the given training
+%   instance.
+%   y_data is a m-by-1 matrix, containing the classification/label of each
+%   training instance.
+%   This method returns a MATLAB SVMStruct object, containing the trained
+%   classifier.
 
-    if (iscell(varargin) && ~isempty(varargin))
-        %Convert to string because varargin is a cell array
-        maximum_number_iterations = cell2mat(varargin);
-        if (maximum_number_iterations <= 0)
-            maximum_number_iterations = 100000;
-        end
-    else
-        maximum_number_iterations = 100000;
-    end
-
-    %Increase the maximum number of iterations! Let the CPU burn!!!
-    options.MaxIter = maximum_number_iterations; 
-    % Simply call svmtrain
-    SVMStruct = svmtrain(x_data, y_data, 'Options', options);
+    SVMStruct = fitcsvm(x_data, y_data);
 end
