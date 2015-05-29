add93a0f1a4cd833bbeb81bf1f2c742eb8da63b4
Joaquim Leitão <jocaleitao93@gmail.com>
2015-05-29 12:45:51 +0100

    Updated classify tree description

--- classify_tree.m
+++ classify_tree.m
@@ -1,5 +1,5 @@
 function [ predicted_values ] = classify_tree( tree, data )
-%UNTITLED4 Classifies a given data set using a trained MATLAB FITCTREE
+%CLASSIFYTREE Classifies a given data set using a trained MATLAB FITCTREE
 %implementation
     predicted_values = predict(tree, data);
 end
