7d854c343403b880c6dcf59c45084ea96e5ef0fb
Joaquim Leitão <jocaleitao93@gmail.com>
2015-05-30 00:45:23 +0100

    Classificaton function for the naive bayes and corrected a bug in the tree classification

---
+++ classify_naive_bayes.m
@@ -0,0 +1,5 @@
+function [ predicted_values ] = classify_naive_bayes(model, data)
+%CLASSIFYNAIVEBAYES Classifies a given data set using a trained MATLAB
+%Naive Bayes Model implementation
+    predicted_values = predict(model, data');
+end
--- classify_tree.m
+++ classify_tree.m
@@ -1,6 +1,6 @@
 function [ predicted_values ] = classify_tree( tree, data )
 %CLASSIFYTREE Classifies a given data set using a trained MATLAB FITCTREE
 %implementation
-    predicted_values = predict(tree, data);
+    predicted_values = predict(tree, data');
 end
 
