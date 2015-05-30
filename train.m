76e10b16b7733822561444a822339b877da5694a
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-05-30 23:27:54 +0100

    train()

---
+++ train.m
@@ -0,0 +1,16 @@
+function model = train(train_sprt_data, method, varargin)
+    model.type =  method;
+    if strcmp(method, 'knn')        
+        model.m = train_knn(train_sprt_data);
+    elseif strcmp(method, 'fitctree')
+        model.m = train_fitctree(train_sprt_data);
+    elseif strcmp(method, 'fitrtree')
+        model.m = train_fitrtree(train_sprt_data);
+    elseif strcmp(method, 'libsvm')
+        model.m = train_libsvm(train_sprt_data);
+    elseif strcmp(method, 'naive_bayes')
+        model.m = train_naive_bayes(train_sprt_data);
+    elseif strcmp(method, 'svm')
+        model.m = train_svm(train_sprt_data);
+    end
+end
