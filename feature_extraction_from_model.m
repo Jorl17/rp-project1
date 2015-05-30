88af54bc8f8b32f0a7989d365d2368a50326f10a
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-05-30 20:17:09 +0100

    Fix missing real conversion

--- feature_extraction_from_model.m
+++ feature_extraction_from_model.m
@@ -4,6 +4,7 @@ function sprt_data = feature_extraction_from_model(sprt_data, m)
         sprt_data.X = real(sprt_data.X);
     elseif strcmp(m.type, 'kernelproj')
         sprt_data = kernelproj(sprt_data, m.m);
+        sprt_data.X = real(sprt_data.X);
     end
     
     sprt_data.num_data = size(sprt_data.X, 2);
