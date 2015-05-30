9a6379f068c7d8e7975f3167c380028247fcba9b
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-05-30 23:30:25 +0100

    build_trained_model()

--- build_trained_model.m
+++ build_trained_model.m
@@ -1,6 +1,5 @@
-function save_trained_model(name, model_train, selected_features, feature_extraction_model, missing_value_method)
+function model_train = build_trained_model(model_train, selected_features, feature_extraction_model, missing_value_method)
     model_train.idx = selected_features;
     model_train.feature_extraction_model = feature_extraction_model;
     model_train.missing_value_method = missing_value_method;
-    save(name, 'model_train')
 end
