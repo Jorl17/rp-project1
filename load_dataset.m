2c64403357ec3a137871cc0b3fb99e23e558c765
Joaquim Leitão <jocaleitao93@gmail.com>
2015-05-31 02:06:09 +0100

    load dataset changed

--- load_dataset.m
+++ load_dataset.m
@@ -1,5 +1,8 @@
-function [ column_names, higgs_data ] = load_dataset()    
-    load('dataset/higgs_data.mat');
+function [ column_names, higgs_data ] = load_dataset( filePath )
+    if nargin < 1
+        filePath = 'dataset/higgs_data.mat';
+    end
+    load(filePath);
     higgs_data=higgs_data_for_optimization(:,2:end);
     column_names = column_names(:,2:end);
 end
