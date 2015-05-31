c78ac15236985ee2c2c782e2d86186a8b49f3d2d
Joaquim Leitão <jocaleitao93@gmail.com>
2015-05-31 11:46:56 +0100

    Corrected bug in the classify_libsvm.m file

--- classify_libsvm.m
+++ classify_libsvm.m
@@ -15,10 +15,10 @@ function [ predicted_label ] = classify_libsvm( svm_model, data, varargin )
     if (iscell(varargin) && ~isempty(varargin))
         testing_label_vector = double(cell2mat(varargin(1)));
     else
-        testing_label_vector = zeros(size(data.X,1), 1); 
+        testing_label_vector = zeros(size(data.X,2), 1); 
     end
     %testing_label_vector is going to be used to compute the accuracy of
     %the prediction
-    [predicted_label, ~, ] = libsvmpredict(testing_label_vector, data.X', svm_model);
+    [predicted_label, ~, ~] = libsvmpredict(testing_label_vector, data.X', svm_model);
 end
 
